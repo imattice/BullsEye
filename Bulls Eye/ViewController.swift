05b0541ce97882d93400aaf06a09d7500f1490e7
Ike <ike.mattice@tiger-sheep.com>
2016-05-24 11:48:43 -0700

    add random number generation for the target value.

--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -10,37 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 50
+    @IBOutlet weak var guessSlider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        guessSlider.value = Float(currentValue)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value was: \(targetValue)"
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
                                       preferredStyle: .Alert)
         let action = UIAlertAction(title: "Nice!",
                                    style: .Default,
                                    handler: nil )
-        
         alert.addAction(action)
-        
         presentViewController(alert, animated: true, completion: nil)
-        
-        
+        startNewRound()
     }
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
     }
-    
-
 }
